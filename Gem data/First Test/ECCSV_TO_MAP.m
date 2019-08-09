8dac4c49a607d19e39e2ad024e81330651c96b9b
95jasmith <52004895+95jasmith@users.noreply.github.com>
2019-08-09 13:58:28 -0500

    Weekly Commit
    Weekly Commit 08/09/2019

--- Gem data/First Test/ECCSV_TO_MAP.m
+++ Gem data/First Test/ECCSV_TO_MAP.m
@@ -1,4 +1,4 @@
-function[High, Low, Avg]= ECCSV_TO_MAP (fileName)
+function[High, Low, Avg, Depth]= ECCSV_TO_MAP (fileName)
 %
 % Opens CSV file with EC data from GEM2 and coverts it to a map of the EC
 % data
@@ -12,6 +12,8 @@ function[High, Low, Avg]= ECCSV_TO_MAP (fileName)
 %             mS per m
 %       Avg: an array with the average value from each frequency and Total
 %             mS per m
+%       Depth: An estimate of the depth of investegation THIS IS NOT THE
+%       ACTUCAL DEPTH AND IS ONLY AN ESTIMATE BASED ON THE SKIN EFFECT
 
 fullFile = importdata(fileName); % opens passed in csv file 
 
@@ -25,11 +27,29 @@ EC18330 = fullFile.data(:,23); % Stores the EC @ 18330 Hz as an array
 EC63030 = fullFile.data(:,24); % Stores the EC @  63030 Hz as an array
 ECT = fullFile.data(:,25);   % Stores the EC total as an array
 
-% creates arrays with max min and average for each frequency and total
-High = [max(EC450); max(EC1530); max(EC5310); max(EC18330); max(EC63030)];
-Low = [min(EC450); min(EC1530); min(EC5310); min(EC18330); min(EC63030)];
-Avg = [mean(EC450); mean(EC1530); mean(EC5310); mean(EC18330); mean(EC63030)];
 
+% creates arrays with max min and average for each frequency and total
+High = [max(EC450); max(EC1530); max(EC5310); max(EC18330); max(EC63030); max(ECT)];
+Low = [min(EC450); min(EC1530); min(EC5310); min(EC18330); min(EC63030); min(ECT)];
+Avg = [mean(EC450); mean(EC1530); mean(EC5310); mean(EC18330); mean(EC63030); mean(ECT)];
+
+% Estimate of depth at 450 Hz in meters
+skinDepth450 = sqrt(2/(Avg(1)*(4*pi*10^(-7))*(2*pi*450)));
+
+% Estimate of depth at 1530 Hz in meters
+skinDepth1530 = sqrt(2/(Avg(2)*(4*pi*10^(-7))*(2*pi*1530)));
+
+% Estimate of depth at 5310 Hz in meters
+skinDepth5310 = sqrt(2/(Avg(3)*(4*pi*10^(-7))*(2*pi*5310)));
+
+% Estimate of depth at 18330 Hz in meters
+skinDepth18330 = sqrt(2/(Avg(4)*(4*pi*10^(-7))*(2*pi*18330)));
+
+% Estimate of depth at 63030 Hz in meters
+skinDepth63030 = sqrt(2/(Avg(5)*(4*pi*10^(-7))*(2*pi*63030)));
+
+% Output array of depth estimates
+Depth = [skinDepth450; skinDepth1530; skinDepth5310; skinDepth18330; skinDepth63030];
 
 xlin = linspace(min(xCoord),max(xCoord),100); % Takes x and y coordinates
 ylin = linspace(min(yCoord),max(xCoord),100); % and converts into meshgrid
@@ -47,26 +67,36 @@ figure(1)
 contourf(X,Y,EC450grid)
 colorbar
 title('Contour Map of EC @ 450 Hz')
+txt = ['Depth: ' num2str(skinDepth450) '(m)'];
+xlabel(txt)
 
 figure(2)
 contourf(X,Y,EC1530grid)
 colorbar
 title('Contour Map of EC @ 1530 Hz')
+txt = ['Depth: ' num2str(skinDepth1530) '(m)'];
+xlabel(txt)
 
 figure(3)
 contourf(X,Y,EC5310grid)
 colorbar
 title('Contour Map of EC @ 5310 Hz')
+txt = ['Depth: ' num2str(skinDepth5310) '(m)'];
+xlabel(txt)
 
 figure(4)
 contourf(X,Y,EC18330grid)
 colorbar
 title('Contour Map of EC @ 18330 Hz')
+txt = ['Depth: ' num2str(skinDepth18330) '(m)'];
+xlabel(txt)
 
 figure(5)
 contourf(X,Y,EC63030grid)
 colorbar
 title('Contour Map of EC @ 63030 Hz')
+txt = ['Depth: ' num2str(skinDepth63030) '(m)'];
+xlabel(txt)
 
 figure(6)
 contourf(X,Y,ECTgrid)
--- Gem data/First Test/INVERETED_TO_MAP.m
+++ Gem data/First Test/INVERETED_TO_MAP.m
@@ -1,4 +1,4 @@
-function[High, Low, Avg]= INVERETED_TO_MAP(fileName, layerNum)
+function[High, Low, Avg, percentKept]= INVERETED_TO_MAP(fileName, layerNum)
 
 
 fullFile = importdata(fileName); % opens passed in csv file
@@ -11,21 +11,29 @@ r = length(fullFile.data(:,end));
 if layerNum == 1
     resLayer1 = fullFile.data(:,10);
 j = [];
+l = [];
     for i = 1:r
         if fullFile.data(i,end) > 10
         j = [j,i];
         end
+        if fullFile.data(i,end) < 20
+            l = [l,i];
+        end
     end
+    percentKept = (r-length(j))/r * 100;
+    percentUnder20 = length(l)/r * 100
     resLayer1(j,:) = [];
     xCoord(j,:) = [];
     yCoord(j,:) = [];
     
+  
     xlin = linspace(min(xCoord),max(xCoord),200); % Takes x and y coordinates
     ylin = linspace(min(yCoord),max(xCoord),200); % and converts into meshgrid
     [X,Y] = meshgrid(xlin,ylin);
     High = max(resLayer1);
     Low = min(resLayer1);
     Avg = mean(resLayer1);
+    %{
     % Converts into griddate
     layer1Grid = griddata(xCoord, yCoord, resLayer1, X, Y, 'cubic');
     
@@ -33,16 +41,23 @@ j = [];
     contourf(X,Y,layer1Grid)
     colorbar
     title('Contour Map of Layer 1')
+    %}
     
 elseif layerNum == 2
     resLayer1 = fullFile.data(:,10);
     resLayer2 = fullFile.data(:,11);
     j = [];
+    l = [];
     for i = 1:r
         if fullFile.data(i,end) > 10
         j = [j,i];
         end
+        if fullFile.data(i,end) < 20
+            l = [l,i];
+        end
     end
+    percentKept = (r-length(j))/r * 100;
+    percentUnder20 = length(l)/r * 100
     resLayer1(j,:) = [];
     resLayer2(j,:) = [];
     xCoord(j,:) = [];
@@ -54,6 +69,7 @@ elseif layerNum == 2
     High = [max(resLayer1); max(resLayer2)];
     Low = [min(resLayer1); min(resLayer2)];
     Avg = [mean(resLayer1); mean(resLayer2)];
+    %{
     % Converts into griddate
     layer1Grid = griddata(xCoord, yCoord, resLayer1, X, Y, 'cubic');
     layer2Grid = griddata(xCoord, yCoord, resLayer2, X, Y, 'cubic'); 
@@ -67,17 +83,24 @@ elseif layerNum == 2
     contourf(X,Y,layer2Grid)
     colorbar
     title('Contour Map of Layer 2')
+    %}
 
 elseif layerNum == 3
     resLayer1 = fullFile.data(:,10);
     resLayer2 = fullFile.data(:,11);
     resLayer3 = fullFile.data(:,12);
 j = [];
+l = [];
     for i = 1:r
         if fullFile.data(i,end) > 10
-        j = [j,i];
+            j = [j,i];
+        end
+        if fullFile.data(i,end) < 20
+            l = [l,i];
         end
     end
+    percentKept = (r-length(j))/r * 100;
+    percentUnder20 = length(l)/r * 100
     resLayer1(j,:) = [];
     resLayer2(j,:) = [];
     resLayer3(j,:) = [];
@@ -91,6 +114,7 @@ j = [];
     High = [max(resLayer1); max(resLayer2); max(resLayer3)];
     Low = [min(resLayer1); min(resLayer2); min(resLayer3)];
     Avg = [mean(resLayer1); mean(resLayer2); mean(resLayer3)];
+    %{
     % Converts into griddate
     layer1Grid = griddata(xCoord, yCoord, resLayer1, X, Y, 'cubic');
     layer2Grid = griddata(xCoord, yCoord, resLayer2, X, Y, 'cubic'); 
@@ -110,6 +134,7 @@ j = [];
     contourf(X,Y,layer3Grid)
     colorbar
     title('Contour Map of Layer 3')
+    %}
     
 else
     print('Invalid number of Layers')
